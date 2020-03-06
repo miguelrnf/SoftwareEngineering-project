723831603a93b7d124eaccd984bd7db5869f02bc
oleks-stopchak <oleksandr.stopchak@tecnico.ulisboa.pt>
2020-03-06 12:12:05 +0000

    define domain define dto define service closes #21

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/post/PostService.java
@@ -0,0 +1,4 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.post;
+
+public class PostService {
+}
---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/post/dto/PostDto.java
@@ -0,0 +1,36 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.post.dto;
+
+public class PostDto {
+    private int id;
+    private PostQuestionDto question;
+    private Boolean postStatus;
+
+    public PostDto(PostQuestionDto question, Boolean postStatus) {
+        this.question = question;
+        this.postStatus = postStatus;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public PostQuestionDto getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(PostQuestionDto question) {
+        this.question = question;
+    }
+
+    public Boolean getPostStatus() {
+        return postStatus;
+    }
+
+    public void setPostStatus(Boolean postStatus) {
+        this.postStatus = postStatus;
+    }
+}
