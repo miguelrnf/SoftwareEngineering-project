b7d40a172109a044dcde72f544b06f28411b7256
oleks-stopchak <oleksandr.stopchak@tecnico.ulisboa.pt>
2020-03-11 17:34:40 +0000

    resolve conflicts

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/post/service/RedirectDuplicatePostTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/post/service/RedirectDuplicatePostTest.groovy
@@ -1,6 +1,5 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.post.service
 
-import javafx.geometry.Pos
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.TestConfiguration
