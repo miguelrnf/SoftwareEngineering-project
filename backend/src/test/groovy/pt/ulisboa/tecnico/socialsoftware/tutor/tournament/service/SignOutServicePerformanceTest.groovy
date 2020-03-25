8c879202c1d03d0ff4f171e7efe2cb652c3777e9
joaoachando <joaoachando@tecnico.ulisboa.pt>
2020-03-25 01:11:43 +0000

    Implement sign out performance tests

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/SignOutServicePerformanceTest.groovy
@@ -0,0 +1,202 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.tournament.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecution
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecutionRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Assessment
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Topic
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.TopicConjunction
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.AssessmentDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.TopicConjunctionDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.TopicDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.AssessmentRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.TopicConjunctionRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.TopicRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.TournamentService
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.domain.Tournament
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.dto.TournamentDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.repository.TournamentRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.UserRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.dto.UserDto
+import spock.lang.Shared
+import spock.lang.Specification
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.TOURNAMENT_NOT_FOUND
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.UNABLE_TO_UNROLL
+
+@DataJpaTest
+class SignOutServicePerformanceTest extends Specification{
+    public static final String COURSE_NAME = "Software Architecture"
+    public static final String ACRONYM = "AS1"
+    public static final String ACADEMIC_TERM = "1 SEM"
+    static final USERNAME_1 = 'username1'
+    static final TITLE = 'first tournament'
+    static final NAME = 'name'
+    static final NUMQUESTIONS = 3
+    static final DATENOW = LocalDateTime.now()
+    static final DATETOMORROW = LocalDateTime.now().plusDays(1)
+
+    @Autowired
+    UserRepository userRepository
+
+    @Autowired
+    CourseRepository courseRepository
+
+    @Autowired
+    CourseExecutionRepository courseExecutionRepository
+
+    @Autowired
+    TournamentService tournamentService
+
+    @Autowired
+    AssessmentRepository assessmentRepository
+
+    @Autowired
+    TopicRepository topicRepository
+
+    @Autowired
+    TopicConjunctionRepository topicConjunctionRepository
+
+    @Shared
+    def tournamentDto
+
+    @Shared
+    def course
+
+    @Shared
+    def courseExecution
+
+    @Shared
+    def creationDate
+
+    @Shared
+    def availableDate
+
+    @Shared
+    def conclusionDate
+
+    @Shared
+    def formatter
+
+    @Shared
+    def STUDENT
+
+    @Shared
+    def assdto
+
+    @Shared
+    def ass
+
+    @Shared
+    def topicDto
+
+    @Shared
+    def topicConjunctionDto
+
+    @Shared
+    def topic
+
+    @Shared
+    def topicConjunction
+
+    def setupSpec() {
+
+        formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+        given: "a quiz"
+        creationDate = LocalDateTime.now()
+        availableDate = LocalDateTime.now()
+        conclusionDate = LocalDateTime.now().plusDays(1)
+        and: "a tournamentDto"
+        tournamentDto = new TournamentDto()
+        tournamentDto.setId(1)
+        tournamentDto.setKey(1)
+        tournamentDto.setStatus(Tournament.TournamentStatus.CREATED)
+        tournamentDto.setAvailableDate(DATENOW.format(formatter))
+        tournamentDto.setConclusionDate(DATETOMORROW.format(formatter))
+        tournamentDto.setNumberOfQuestions(NUMQUESTIONS)
+
+        and: "a user with the role student"
+        STUDENT = new User()
+        STUDENT.setId(1)
+        STUDENT.setRole(User.Role.STUDENT)
+        STUDENT.setUsername(USERNAME_1)
+
+    }
+
+    def setup() {
+        given: "a course and a course execution"
+        course = new Course(COURSE_NAME, Course.Type.TECNICO)
+        courseExecution = new CourseExecution(course, ACRONYM, ACADEMIC_TERM, Course.Type.TECNICO)
+        tournamentDto.setAssessmentDto(assdto)
+
+        and: "a topic dto"
+        topicDto = new TopicDto()
+        topicDto.setId(1)
+        topicDto.setName(NAME)
+
+        and: "a topic conjunction dto"
+        topicConjunctionDto = new TopicConjunctionDto()
+        topicConjunctionDto.setId(1)
+        topicConjunctionDto.addTopic(topicDto)
+
+        and: " a valid assessments"
+        assdto = new AssessmentDto()
+        assdto.setId(1)
+        assdto.setStatus(Assessment.Status.AVAILABLE.name())
+        assdto.setTopicConjunctionsFromUnit(topicConjunctionDto)
+        topic = new Topic(course, topicDto)
+        topicConjunction = new TopicConjunction()
+
+        and:
+        def tcl = new ArrayList<TopicConjunction>()
+        tcl.add(topicConjunction)
+        ass = new Assessment(courseExecution, tcl, assdto)
+
+        and: "a user with the role student"
+        def userS = new User('name', USERNAME_1, 1, User.Role.STUDENT)
+
+        then:"add to repository"
+        courseRepository.save(course)
+        courseExecutionRepository.save(courseExecution)
+        userRepository.save(userS)
+        topicRepository.save(topic)
+        topicConjunctionRepository.save(topicConjunction)
+        assessmentRepository.save(ass)
+
+    }
+
+    def "valid course Execution"() {
+        given:
+        tournamentDto.setOwner(new UserDto(STUDENT))
+        tournamentDto.setTitle(TITLE)
+        tournamentDto.setId(1)
+        tournamentDto.setAssessmentDto(assdto)
+        1.upto(5, {tournamentService.createTournament(courseExecution.getId(), tournamentDto)})
+        1.upto(5, {tournamentService.enrollStudent(courseExecution.getId(), STUDENT.getUsername(), it)})
+
+        when:
+        1.upto(5, {tournamentService.unrollStudent(STUDENT.getUsername(), it)})
+
+        then:
+        true
+    }
+
+    @TestConfiguration
+    static class TournamentServiceImplTestContextConfiguration {
+
+        @Bean
+        TournamentService tournamentService() {
+            return new TournamentService()
+        }
+    }
+}
