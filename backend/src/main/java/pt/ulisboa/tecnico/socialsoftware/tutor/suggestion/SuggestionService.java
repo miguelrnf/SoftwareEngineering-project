f3ff1d952bf1eeb146e8d995f7ddb3326b80d35a
Ricardo Vaz <50366220+ricardovazz@users.noreply.github.com>
2020-05-24 15:16:46 +0100

    Update backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/suggestion/SuggestionService.java
    Co-authored-by: oleks-stopchak <47886918+oleks-stopchak@users.noreply.github.com>

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/suggestion/SuggestionService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/suggestion/SuggestionService.java
@@ -265,7 +265,7 @@ public class SuggestionService {
             backoff = @Backoff(delay = 5000))
     @Transactional(isolation = Isolation.REPEATABLE_READ)
     public SuggestionDto setCheckMark(int courseExecutionId, SuggestionDto suggestionDto, String username){
-        String username = userDto.getUsername();
+       
         courseExecutionRepository.findById(courseExecutionId).orElseThrow(() -> new TutorException(COURSE_NOT_FOUND, courseExecutionId));
         User user = checkIfUserExists(username);
 
